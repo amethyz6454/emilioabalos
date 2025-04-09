acac123afc7bad1a18261a8b2303f0664b711327
Emilio Abalos <emilioabalos@outlook.com>
2025-04-09 14:28:46 +0800

    cofig fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import MainLayout from "@/layouts/MainLayout";
 import HeroSection from "@/sections/HeroSection";
 import ProjectsSection from "@/sections/ProjectsSection";
+import ContainerStyle from "@/styles/components/container.module.scss";
 import { NextPage } from "next";
 import { JSX } from "react";
-import ContainerStyle from "@/styles/components/container.module.scss";
 
 const Home: NextPage = (): JSX.Element => {
     return (
