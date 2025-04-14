bc3340f282ab04fe71c763af7fdae0750788dcc3
Emilio Abalos <emilioabalos@outlook.com>
2025-04-14 20:10:04 +0800

    proper placement of metadatas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.scss";
 import "@/styles/root.scss";
 import type { AppProps } from "next/app";
 import { Sofia_Sans } from "next/font/google";
+import Head from "next/head";
 import { useEffect } from "react";
 
 const sofiaSans = Sofia_Sans({ subsets: ["latin"] });
@@ -24,6 +25,28 @@ export default function App({ Component, pageProps }: AppProps) {
                     }
                 `}
             </style>
+            <Head>
+                <meta
+                    name="description"
+                    content="Emilio Abalos's portfolio showcasing web design and front-end development projects. View my work and contact me for collaborations."
+                />
+                <meta property="og:title" content="Emilio Abalos" />
+                <meta
+                    property="og:description"
+                    content="Emilio Abalos's portfolio showcasing web design and front-end development projects. View my work and contact me for collaborations."
+                />
+                <meta property="og:image" content="/thumbnail.jpg" />
+                <meta property="og:url" content="https://emilioabalos.vercel.app/" />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Emilio Abalos" />
+                <meta
+                    name="twitter:description"
+                    content="Emilio Abalos's portfolio showcasing web design and front-end development projects. View my work and contact me for collaborations."
+                />
+                <meta name="twitter:image" content="/thumbnail.jpg" />
+                <link rel="icon" href="/app-logo.png" as="image" />
+            </Head>
             <Component {...pageProps} />
         </>
     );
