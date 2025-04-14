8a3b92065a9484e65e5003b7c00e3bf12ab830ca
Emilio Abalos <emilioabalos@outlook.com>
2025-04-14 20:06:54 +0800

    viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta
                     name="description"
                     content="Emilio Abalos's portfolio showcasing web design and front-end development projects. View my work and contact me for collaborations."
